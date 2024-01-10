0b94bcc3c4e4b42df11f7b68855afbf4de682534
Jérémy Laurent <poulpy2k@gmail.com>
2024-01-10 09:13:03 +0100

    Initial Commit

---
+++ SwiftUI-M2/SwiftUI_M2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_M2App.swift
+//  SwiftUI-M2
+//
+//  Created by Jérémy Laurent on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_M2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
